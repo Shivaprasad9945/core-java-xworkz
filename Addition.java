f3dc2351e4b322d9079bdf972e272e2529501875
Shivaprasad9945 <prasadshiva46792@gmail.com>
2023-07-13 13:04:19 +0530

    adding addition file

---
+++ Addition.java
@@ -0,0 +1,18 @@
+ class Addition{
+public static void main(String addition[]){
+//10 Addition
+add(20,30,40);
+add(27,48,78);
+add(21,65,70);
+add(46,55,60);
+add(50,32,52);
+add(60,72,64);
+add(11,12,18);
+add(15,74,22);
+add(16,80,90);
+add(86,76,88);
+}
+public static void add(int c,int d,int e){
+	System.out.println(c+d+e);
+}
+}
