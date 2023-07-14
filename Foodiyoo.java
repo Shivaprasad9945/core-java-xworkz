4fb4cc75b7a2e4775285e4e1d6fe4f921d3372b9
Shivaprasad9945 <prasadshiva46792@gmail.com>
2023-07-14 11:57:44 +0530

    added foodiye to methods

---
+++ Foodiyoo.java
@@ -0,0 +1,230 @@
+class Foodiyoo{
+public static double search(String foodName){
+
+		if("aloo tikki chat"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 275.00;
+		}
+		if("papdi chaat"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 277.00;
+		}
+		if("gol gappe"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 285.00;
+		}
+		if("bhelpuri"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 50.00;
+		}
+		if("chicken tikka chaat"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 385.00;
+			
+		}
+		if("dhai bhalla"==foodName){
+			System.out.println("searched food name is"+foodName);
+		return 299.00;
+		
+		}
+		if("fish head curry"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00;
+		}
+		if("chicken masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00;
+		}
+		if("mutton masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00;
+		}
+		if("ghobi manchurian"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 60.00;
+		}
+		if("chilli fish"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 200.00;
+		}
+		if("fish masala"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 150.00;
+			
+		}
+		if("prawn masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+		return 200.00;
+		
+		}
+		if("mutton korma"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00;
+		}
+		if("butter chicken"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 250.00;
+		}
+		if("noodles"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 150.00;
+		}
+		if("tandoori prawn"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 199.00;
+		}
+		if("mutton fried"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 150.00;
+		}
+		if("kharabath"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 70.00;
+		}
+		if ("ragimudde"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 40.00;
+			}
+		if("ragiidly"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 50.00;
+			}
+		
+		return 0.0;
+}
+
+
+
+
+
+			
+public static double search(String foodName,int quantity){
+
+		if("aloo tikki chat"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 275.00*quantity;
+		}
+		if("papdi chaat"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 277.00*quantity;
+		}
+		if("gol gappe"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 285.00*quantity;
+		}
+		if("bhelpuri"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 50.00*quantity;
+		}
+		if("chicken tikka chaat"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 385.00*quantity;
+			
+		}
+		if("dhai bhalla"==foodName){
+			System.out.println("searched food name is"+foodName);
+		return 299.00*quantity*quantity;
+		
+		}
+		if("fish head curry"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00*quantity;
+		}
+		if("chicken masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00*quantity;
+		}
+		if("mutton masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00*quantity;
+		}
+		if("ghobi manchurian"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 60.00*quantity;
+		}
+		if("chilli fish"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 200.00*quantity;
+		}
+		if("fish masala"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 150.00*quantity;
+			
+		}
+		if("prawn masala"==foodName){
+			System.out.println("searched food name is"+foodName);
+		return 200.00*quantity;
+		
+		}
+		if("mutton korma"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 200.00*quantity;
+		}
+		if("butter chicken"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 250.00*quantity;
+		}
+		if("noodles"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 150.00*quantity;
+		}
+		if("tandoori prawn"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 199.00*quantity;
+		}
+		if("mutton fried"==foodName){
+			System.out.println("searced food name is"+foodName);
+			return 150.00*quantity;
+		}
+		if("kharabath"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 70.00*quantity;
+		}
+		if ("ragimudde"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 40.00*quantity;
+			}
+		if("ragiidly"==foodName){
+			System.out.println("searched food name is"+foodName);
+			return 50.00*quantity;
+			}
+		
+		return 0.0*quantity;
+}
+}
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
